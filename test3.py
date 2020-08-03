f9535121cc6c36056947117d3a11a99edb84b92d
rxpha <rapha.diogenes@gmail.com>
2020-08-03 17:17:00 -0400

    update

---
+++ test3.py
@@ -0,0 +1,19 @@
+# digits_list = input()
+# digits_list = [int(n) for n in digits_list]
+# digits_list = [sum(digits_list) / len(digits_list)]
+# print(digits_list[0])
+
+# words = ['apple', 'pear', 'banana', 'Ananas']
+# basket = [fruit for fruit in words if fruit[0] == 'a' or fruit[0] == 'A']
+# print(basket)
+
+# M = [[34, 77, 8,  45],
+#      [80, 15, 23, 57],
+#      [92, 86, 19, 38]]
+
+# elem = M[2][0]
+# print(elem)
+
+students = [["Will", "B"], ["Kate", "B"], ["Max", "A"], ["Elsa", "C"], ["Alex", "B"], ["Chris", "A"]]
+# get only students with 'A' name/grade
+# do all this in one line
